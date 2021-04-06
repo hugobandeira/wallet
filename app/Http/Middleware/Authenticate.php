32c44a9a1163e4d02a65d98f557bdd9ed6790d59
HugoBandeira <hugo.bandeira@hotmail.com>
2021-04-06 03:16:12 -0300

    fix import

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,7 +13,7 @@ class Authenticate extends Middleware
      * @param  Request  $request
      * @return string|null
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request): ?string
     {
         if (!$request->expectsJson()) {
             return route('login');
