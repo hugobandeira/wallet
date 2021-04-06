9e94989dfc6dfce2f6eaa174534074c813210665
HugoBandeira <hugo.bandeira@hotmail.com>
2021-04-06 20:54:00 -0300

    Fix seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(
             [
-                TransactionsSeeder::class,
                 WalletSeeder::class,
             ]
         );
